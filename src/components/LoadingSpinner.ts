d267bdd04652d6efd165f112ff500db4b6054d06
NR0617 <nara20210617@gmail.com>
2023-08-31 18:03:25 +0900

    feat: 파일명 변경

--- src/components/LoadingSpinner.ts
+++ src/components/LoadingSpinner.ts
@@ -1,12 +1,12 @@
 import styled, { keyframes } from 'styled-components';
 
 const rotation = keyframes`
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 `;
 
 const Spinner = styled.div`
