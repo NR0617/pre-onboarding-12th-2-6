fa3f73ed8efef3288bba9c1cc15048d317a29274
NR0617 <nara20210617@gmail.com>
2023-08-31 15:40:55 +0900

    feat: 디테일 페이지 태그 추가

--- src/pages/RepoItemPage.tsx
+++ src/pages/RepoItemPage.tsx
@@ -4,18 +4,46 @@ import { callIssueDetail } from 'utils/octokit';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import remarkGfm from 'remark-gfm';
 
+interface DetailData {
+  html: string;
+  itemNumber: number;
+  itemTitle: string;
+  username: string;
+  createdAt: string;
+  commentCount: number;
+  userProfileImage: string;
+}
+
 const RepoItemPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const query = new URLSearchParams(location.search);
   const issueNumber = query.get('number');
 
-  const [html, setHtml] = useState('');
+  const [pageDetailData, setPageDetailData] = useState<DetailData>({
+    html: '',
+    itemNumber: NaN,
+    itemTitle: '',
+    username: '',
+    createdAt: '',
+    commentCount: NaN,
+    userProfileImage: '',
+  });
   useEffect(() => {
     if (issueNumber === null) navigate('/');
     callIssueDetail(issueNumber)
       .then((res) => {
-        setHtml(res.body);
+        console.log(res);
+        const data = {
+          html: res.body,
+          itemNumber: res.number,
+          itemTitle: res.title,
+          username: res.user.login,
+          createdAt: res.created_at,
+          commentCount: res.comments,
+          userProfileImage: res.user.avatar_url,
+        };
+        setPageDetailData({ ...data });
       })
       .catch(() => {});
   }, [issueNumber]);
@@ -23,11 +51,15 @@ const RepoItemPage = () => {
   return (
     <div>
       <p>RepoItemPage</p>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{html}</ReactMarkdown>
+      <img src={pageDetailData.userProfileImage} alt="" />
+      <span>{pageDetailData.username}</span>
+      <span>{pageDetailData.itemNumber}</span>
+      <span>{pageDetailData.itemTitle}</span>
+      <span>{pageDetailData.createdAt}</span>
+      <span>{pageDetailData.commentCount}</span>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{pageDetailData?.html}</ReactMarkdown>
     </div>
   );
 };
 
 export default RepoItemPage;
-
-// { itemId, itemTitle, username, createdAt, commentCount, userProfileImage, itemContent }
